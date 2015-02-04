b2763e6495f532e6d043532d6deffff056f4b729
fr4ed <fr4ed@rustadholmboe.com>
2015-02-04 20:29:56 +0100

    changed back!?

--- AS_PS_Demo.R
+++ AS_PS_Demo.R
@@ -8,7 +8,7 @@ png(file="AS_ICER.png",width=900,height=620)
 # x=ICER, y=AS, size=Budsjett
 ggplot(table, aes(x=ICER, y=AS, label=Name, legend=FALSE, group=Budsjett))+
   geom_vline(xintercept=0, color="blue",alpha=0.5)+
-  geom_hline(yintercept=0, color="blue",alpha=0.5)+
+  geom_hline(yintercept=0, color="red",alpha=0.5)+
   geom_point(size=20,colour="black", fill="gray", alpha=0.3, shape=1)+
   geom_point(aes(size=Budsjett),colour="black", fill="green", shape=21)+
   geom_text(size=4, hjust=-0.3, vjust=-0.4)#,position=position_jitter(height=0.3))
